fbbd4e3a124e3ed56de5adad9e7c78a05016d309
Edin Mavric <edinmavric10@gmail.com>
2025-03-01 23:04:46 +0100

    More bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,28 @@ import Lights from './components/Lights';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div className="flex flex-col bg-gray-900">
-            <div className="flex flex-col items-center justify-center">
-                <Navbar />
-                <Hero />
-                <About />
+            <div className="flex items-center justify-center">
+                <div className="flex flex-col items-center justify-center max-sm:w-[90%]">
+                    <Navbar />
+                    <Hero />
+                    <About />
+                </div>
             </div>
 
             <div className="h-[100dvh]">
@@ -22,7 +36,8 @@ export default function App() {
                     <OrbitControls
                         enablePan={false}
                         enableZoom={false}
-                        zoomSpeed={0.4}
+                        enableRotate={!isMobile}
+                        rotateSpeed={0.4}
                     />
                     <Model />
                 </Canvas>
